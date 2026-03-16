a0e60c06a24831de69b8b9aa8abb30049cc68062
andfelipeme <andfelipeme@gmail.com>
2026-03-16 10:10:03 -0500

    Error: Se arreglar robots.txt y se agrega sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import react from "@astrojs/react";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap()],
 });
